7af0945798b81465984e04d6bef1f83c7ee6715e
ohs80340 <ohs80340@ohs80340noMacBook-Air.local>
2021-01-24 13:33:34 +0900

    separatethread

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
+// 次の例は、HTTPサーバーを別のスレッドで起動する方法を示しています。
 
-
-// HttpServerタイプは、HTTPリクエストの処理を担当します。
-
-// HttpServerは、アプリケーションファクトリをパラメータとして受け入れ、
-// アプリケーションファクトリには送信と同期の境界が必要です。
-// 詳細については、マルチスレッドのセクションをご覧ください。
-
-// 特定のソケットアドレスにバインドするには、bind（）を使用する必要があり、複数回呼び出すことができます。
-//  sslソケットをバインドするには、bind_openssl（）またはbind_rustls（）を使用する必要があります。 
-// HTTPサーバーを実行するには、HttpServer :: run（）メソッドを使用します。
-
-use actix_web::{ web, App, HttpResponse, HttpServer };
+use actix_web::{rt::System, web, App, HttpResponse, HttpServer};
+use std::sync::mpsc;
+use std::thread;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new( || {
-        App::new().route("/", web::get().to(|| HttpResponse::Ok()))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+async fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let sys = System::new("http-server");
+
+        let srv = HttpServer::new(|| {
+            App::new().route("/", web::get().to(|| HttpResponse::Ok()))
+        })
+        .bind("127.0.0.1:8080")?
+        .shutdown_timeout(60) // <- Set shutdown timeout to 60 seconds
+        .run();
+
+        let _ = tx.send(srv);
+        sys.run()
+    });
+
+    let srv = rx.recv().unwrap();
+
+    // 新しい接続の受け入れを一時停止します
+    srv.pause().await;
+
+    // 新しい接続の受け入れを再開します
+    srv.resume().await;
+
+    // サーバーを停止します
+    srv.stop(true).await;
+
 }
-
-// run（）メソッドは、サーバータイプのインスタンスを返します。
-// サーバータイプのメソッドは、HTTPサーバーの管理に使用できます
-
-// pause（）-着信接続の受け入れを一時停止します
-// resume（）-着信接続の受け入れを再開します
-// stop（）-着信接続処理を停止し、すべてのワーカーを停止して終了します
-
