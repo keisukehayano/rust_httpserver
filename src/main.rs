7f94890453552deb8ef0400e7dc91268f9a9a793
ohs80340 <ohs80340@ohs80340noMacBook-Air.local>
2021-02-06 11:14:06 +0900

    StreamingResponseBody

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,19 @@
-// Actixは、キープアライブ接続で要求を待機できます。
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer};
+use futures::{future::ok, stream::once};
 
-// 存続接続の動作は、サーバー設定によって定義されます。
+#[get("/stream")]
+async fn stream() -> HttpResponse {
+    let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
 
-// 75、Some（75）、KeepAlive :: Timeout（75）-75秒のキープアライブタイマーを有効にします。
-// NoneまたはKeepAlive :: Disabled-キープアライブを無効にします。
-// KeepAlive :: Tcp（75）-SO_KEEPALIVEソケットオプションを使用します。
-
-use actix_web::{ http, web, App, HttpResponse, HttpServer, HttpRequest };
-
-#[actix_web::main]
-async fn main() ->std::io::Result<()> {
-    let one = HttpServer::new(|| {
-        App::new().route("/", web::get().to(|| HttpResponse::Ok()))
-    })
-    .keep_alive(75);            // <- Set keep-alive to 75 seconds
-
-    // let _two = HttpServer::new(|| {
-    //     App::new().route("/", web::get().to(|| HttpResponse::Ok()))
-    // })
-    // .keep_alive(); // <- Use `SO_KEEPALIVE` socket option.
-
-    let three = HttpServer::new(|| {
-        App::new().route("/", web::get().to(|| HttpResponse::Ok()))
-    })
-    .keep_alive(None);
-    
-
-  one.bind("127.0.0.1:8080")?.run().await
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming(body)
 }
 
-// 上記の最初のオプションが選択されている場合、キープアライブ状態は応答の接続タイプに基づいて計算されます。
-// デフォルトでは、HttpResponse :: connection_typeは定義されていません。
-// その場合、キープアライブはリクエストのHTTPバージョンによって定義されます。
-
-// keep alive is off for HTTP/1.0 and is on for HTTP/1.1 and HTTP/2.0.
-
-// 接続タイプは、HttpResponseBuilder :: connection_type（）メソッドを使用して変更できます。
-
-//async fn index(req: HttpRequest) -> HttpResponse {
-//    HttpResponse::Ok()
-//        .connection_type(http::ConnectionType::Close) // <- Close connection
-//        .force_close() // <- Alternative method
-//        .finish()
-//}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(stream))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
